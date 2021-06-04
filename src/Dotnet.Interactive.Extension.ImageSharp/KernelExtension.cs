f61ee2d04aa32c27f562df7abed3350fdfe83a1d
Diego Colombo <dicolomb@microsoft.com>
2021-06-05 00:18:26 +0100

    support animated gif
    simplify formatting
    
    
    Fxix error

--- src/Dotnet.Interactive.Extension.ImageSharp/KernelExtension.cs
+++ src/Dotnet.Interactive.Extension.ImageSharp/KernelExtension.cs
@@ -1,11 +1,12 @@
 ﻿using System;
-using System.IO;
 using System.Threading.Tasks;
 
 using Microsoft.DotNet.Interactive;
 using Microsoft.DotNet.Interactive.Commands;
 using Microsoft.DotNet.Interactive.Formatting;
 using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats;
+using SixLabors.ImageSharp.Formats.Gif;
 using SixLabors.ImageSharp.Formats.Png;
 
 using static Microsoft.DotNet.Interactive.Formatting.PocketViewTags;
@@ -19,8 +20,8 @@ namespace Dotnet.Interactive.Extension.ImageSharp
             Formatter.Register<Image>((image, writer) =>
             {
                 var id = Guid.NewGuid().ToString("N");
-                var data = GetImageBytes(image);
-                var imgTag = CreateImgTag(data, id, image.Height, image.Width);
+     
+                var imgTag = CreateImgTag(image, id, image.Height, image.Width);
                 writer.Write(imgTag);
             }, HtmlFormatter.MimeType);
 
@@ -30,18 +31,12 @@ namespace Dotnet.Interactive.Extension.ImageSharp
                     $"Added support for SixLabors.ImageSharp to kernel {kernel.Name}.")));
         }
 
-        private static byte[] GetImageBytes(Image image)
+        private static PocketView CreateImgTag(Image image, string id, int height, int width)
         {
-            using var stream = new MemoryStream();
-            image.Save(stream, new PngEncoder());
-            stream.Flush();
-            var data = stream.ToArray();
-            return data;
-        }
+            var format = image.Frames.Count > 1 ? (IImageFormat)GifFormat.Instance : PngFormat.Instance;
+            var data = image.ToBase64String(format);
+            var imageSource = $"data:{format.DefaultMimeType};base64, {data}";
 
-        private static PocketView CreateImgTag(byte[] data, string id, int height, int width)
-        {
-            var imageSource = $"data:image/png;base64, {Convert.ToBase64String(data)}";
             PocketView imgTag = img[id: id, src: imageSource, height: height, width: width]();
             return imgTag;
         }
