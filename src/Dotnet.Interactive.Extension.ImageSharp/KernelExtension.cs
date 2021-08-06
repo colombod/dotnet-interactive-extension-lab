c7ffcbcbbba4167d2e7e40d9aa79587091fdfad5
colombod <colombod@me.com>
2021-08-06 20:59:31 +0100

    update pacakges

--- src/Dotnet.Interactive.Extension.ImageSharp/KernelExtension.cs
+++ src/Dotnet.Interactive.Extension.ImageSharp/KernelExtension.cs
@@ -8,35 +8,46 @@ using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.Formats;
 using SixLabors.ImageSharp.Formats.Gif;
 using SixLabors.ImageSharp.Formats.Png;
-
 using static Microsoft.DotNet.Interactive.Formatting.PocketViewTags;
 
 namespace Dotnet.Interactive.Extension.ImageSharp
 {
+    /// <summary>
+    /// A <see cref="IKernelExtension"/> implementation adding support for ImageSharp images.
+    /// </summary>
     public class KernelExtension : IKernelExtension
     {
-        public async Task OnLoadAsync(Kernel kernel)
+        /// <inheritdoc/>
+        public Task OnLoadAsync(Kernel kernel)
         {
-            Formatter.Register<Image>((image, writer) =>
-            {
-                var id = Guid.NewGuid().ToString("N");
-     
-                var imgTag = CreateImgTag(image, id, image.Height, image.Width);
-                writer.Write(imgTag);
-            }, HtmlFormatter.MimeType);
+            RegisterFormatters();
 
-            await kernel.SendAsync(
+            return kernel.SendAsync(
                 new DisplayValue(new FormattedValue(
                     "text/markdown",
                     $"Added support for SixLabors.ImageSharp to kernel {kernel.Name}.")));
         }
 
+        /// <summary>
+        /// Registers the formatters.
+        /// </summary>
+        public static void RegisterFormatters() => Formatter.Register<Image>(
+            (image, writer) =>
+            {
+                var id = Guid.NewGuid().ToString("N");
+                var imgTag = CreateImgTag(image, id, image.Height, image.Width);
+                writer.Write(imgTag);
+            }, HtmlFormatter.MimeType);
+
         private static PocketView CreateImgTag(Image image, string id, int height, int width)
         {
-            var format = image.Frames.Count > 1 ? (IImageFormat)GifFormat.Instance : PngFormat.Instance;
+            var format = image.Frames.Count > 1
+                ? (IImageFormat)GifFormat.Instance
+                : PngFormat.Instance;
+
             var imageSource = image.ToBase64String(format);
-            PocketView imgTag = img[id: id, src: imageSource, height: height, width: width]();
-            return imgTag;
+
+            return (PocketView)img[id: id, src: imageSource, height: height, width: width]();
         }
     }
 }
