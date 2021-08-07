eb4f7c5c9cd94b45b432a5a22cfa72b103ecad1a
colombod <colombod@me.com>
2021-08-07 01:17:05 +0100

    color  swatch

--- src/Dotnet.Interactive.Extension.ImageSharp/KernelExtension.cs
+++ src/Dotnet.Interactive.Extension.ImageSharp/KernelExtension.cs
@@ -4,10 +4,15 @@ using System.Threading.Tasks;
 using Microsoft.DotNet.Interactive;
 using Microsoft.DotNet.Interactive.Commands;
 using Microsoft.DotNet.Interactive.Formatting;
+
 using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Drawing.Processing;
 using SixLabors.ImageSharp.Formats;
 using SixLabors.ImageSharp.Formats.Gif;
 using SixLabors.ImageSharp.Formats.Png;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+
 using static Microsoft.DotNet.Interactive.Formatting.PocketViewTags;
 
 namespace Dotnet.Interactive.Extension.ImageSharp
@@ -31,13 +36,43 @@ namespace Dotnet.Interactive.Extension.ImageSharp
         /// <summary>
         /// Registers the formatters.
         /// </summary>
-        public static void RegisterFormatters() => Formatter.Register<Image>(
-            (image, writer) =>
+        public static void RegisterFormatters()
+        {
+            Formatter.Register<Image>(
+                (image, writer) =>
+                {
+                    var id = Guid.NewGuid().ToString("N");
+                    var imgTag = CreateImgTag(image, id, image.Height, image.Width);
+                    writer.Write(imgTag);
+                }, HtmlFormatter.MimeType);
+
+
+            Formatter.Register<Color>((color, writer) =>
+
             {
+                var img = new Image<Rgba32>(36, 24, color);
+                img.Mutate(c =>
+                {
+                    c.DrawPolygon(Color.Black, 2.0f, new[]
+                    {
+                        new PointF(0f, 0f),
+                        new PointF(img.Width, 0f),
+                        new PointF(img.Width, img.Height),
+                        new PointF(0f, img.Height),
+                    });
+                });
+
                 var id = Guid.NewGuid().ToString("N");
-                var imgTag = CreateImgTag(image, id, image.Height, image.Width);
-                writer.Write(imgTag);
+                var imgTag = CreateImgTag(img, id, img.Height, img.Width);
+
+                PocketView colorSwatch = div(table(tr(
+                    td(imgTag),
+                    td(color.ToString())))
+                );
+                writer.Write(colorSwatch.ToDisplayString());
+
             }, HtmlFormatter.MimeType);
+        }
 
         private static PocketView CreateImgTag(Image image, string id, int height, int width)
         {
