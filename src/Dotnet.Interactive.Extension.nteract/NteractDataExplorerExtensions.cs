843bb53c6146d86e8c6c903a4cb2bc70b72e0285
Diego Colombo <dicolomb@microsoft.com>
2022-01-28 00:08:10 +0000

    tests

---
+++ src/Dotnet.Interactive.Extension.nteract/NteractDataExplorerExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.DotNet.Interactive;
+using Microsoft.DotNet.Interactive.Formatting.TabularData;
+
+namespace Dotnet.Interactive.Extension.nteract;
+
+public static class NteractDataExplorerExtensions
+{
+    public static T UseNteractDataExplorer<T>(this T kernel, Uri libraryUri = null, string libraryVersion = null, string cacheBuster = null) where T : Kernel
+    {
+        NteractDataExplorer.RegisterFormatters();
+        NteractDataExplorer.SetDefaultConfiguration(libraryUri, libraryVersion, cacheBuster);
+        DataExplorer.Register<TabularDataResource, NteractDataExplorer>();
+        return kernel;
+    }
+}
