7b8e09e82dfaa21b37688c4f3a5a292dc5cb47c3
Diego Colombo <dicolomb@microsoft.com>
2022-02-08 11:15:49 +0000

    refactor idataview support to single extension

---
+++ src/DataView.InteractiveExtension/KernelExtension.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+
+using Microsoft.DotNet.Interactive;
+using Microsoft.DotNet.Interactive.Commands;
+
+namespace DataView.InteractiveExtension;
+
+public class KernelExtension : IKernelExtension
+{
+    /// <inheritdoc/>
+    public Task OnLoadAsync(Kernel kernel)
+    {
+
+        return kernel.SendAsync(
+            new DisplayValue(new FormattedValue(
+                "text/markdown",
+                $"Added support IDataView to kernel {kernel.Name}.")));
+    }
+
+}
