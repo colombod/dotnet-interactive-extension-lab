b64f69cc41474dbac2bc248c894ed3979c99ea59
Diego Colombo <dicolomb@microsoft.com>
2022-06-20 16:54:19 +0100

    add zoom + pan
    add zoom

--- src/DotLanguage.InteractiveExtension/DotLanguageKernel.cs
+++ src/DotLanguage.InteractiveExtension/DotLanguageKernel.cs
@@ -42,19 +42,20 @@ internal class DotLanguageKernel : Kernel,
     private IHtmlContent GenerateHtml(string commandCode, Uri libraryUri, Uri wasmFolder, string? libraryVersion, string cacheBuster, string? width, string? height)
     {
         var requireUri = new Uri("https://cdnjs.cloudflare.com/ajax/libs/require.js/2.3.6/require.min.js");
-        var divId = Guid.NewGuid().ToString("N");
+        var divId = $"hppc_{Guid.NewGuid().ToString("N")}";
         var code = new StringBuilder();
         var renderingFunctionName = $"loadHpcc_{divId}";
-        code.AppendLine("<div >");
-
+        code.AppendLine("<div>");
+        // the svg part
+        code.AppendLine($"<div id=\"container_{divId}\">");
         code.AppendLine($"<div id=\"{divId}\" style=\"height:{height}; width:{width}\"></div>");
+        code.AppendLine("</div>");
 
+        // the script part
         code.AppendLine(@"<script type=""text/javascript"" defer>");
         AppendJsCode(code, divId, renderingFunctionName, libraryUri, wasmFolder, libraryVersion, cacheBuster, commandCode);
         code.AppendLine(JavascriptUtilities.GetCodeForEnsureRequireJs(requireUri, renderingFunctionName));
         code.AppendLine("</script>");
-
-       
         code.AppendLine("</div>");
 
         var html = new HtmlString(code.ToString());
@@ -71,14 +72,32 @@ internal class DotLanguageKernel : Kernel,
         var libraryAbsoluteUri = Regex.Replace(libraryUri.AbsoluteUri, @"(\.js)$", string.Empty);
         cacheBuster ??= Guid.NewGuid().ToString("N");
         stringBuilder.AppendLine($@" 
-        (require.config({{ 'paths': {{ 'context': '{libraryVersion}', 'hpcc' : '{libraryAbsoluteUri}', 'urlArgs': 'cacheBuster={cacheBuster}' }}}}) || require)(['hpcc'], (hpcc) => {{");
+        (require.config({{ 'paths': {{ 'context': '{libraryVersion}' , 'd3': 'https://cdn.jsdelivr.net/npm/d3@7.4.4/dist/d3.min', 'hpcc' : '{libraryAbsoluteUri}', 'urlArgs': 'cacheBuster={cacheBuster}' }}}}) || require)(['d3','hpcc'], (d3, hpcc) => {{");
 
 
         stringBuilder.AppendLine($@"
             let container = document.getElementById('{divId}');
             let dot = `{code}`;
             hpcc.wasmFolder(`{wasmFolder.AbsoluteUri}`);
-            hpcc.graphviz.layout(dot, ""svg"", ""dot"").then(svg => {{ container.innerHTML = svg; }});
+            hpcc.graphviz.layout(dot, ""svg"", ""dot"").then(svg => {{ 
+                d3.select('#container_{divId}').html(svg);
+                svg = d3.select('#container_{divId}').select('svg');
+                let g = svg.select('g');
+                let [x, y, width, height] = svg.attr('viewBox').split(' ');
+                let zoom = d3.zoom();
+                svg.call(zoom
+                .extent([
+                    [0, 0],
+                    [width, height]
+                ])
+                .scaleExtent([0.1, 8])
+                .on(""zoom"", ({{
+                    transform
+                }}) => g.attr('transform', transform))
+            );
+            svg.call(zoom.translateTo, width / 2, -height / 2);
+            container.innerHTML = svg; 
+            }});
         }},
         (error) => {{
             console.log(error);
