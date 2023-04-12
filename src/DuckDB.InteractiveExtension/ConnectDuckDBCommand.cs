e8d9442df322dd287f55d2eb15db75724ab1a206
Diego Colombo <colombod@me.com>
2023-04-12 13:32:21 +0100

    starting point

---
+++ src/DuckDB.InteractiveExtension/ConnectDuckDBCommand.cs
@@ -0,0 +1,29 @@
+﻿using System.CommandLine;
+using System.CommandLine.Invocation;
+using System.Threading.Tasks;
+using Microsoft.DotNet.Interactive;
+using Microsoft.DotNet.Interactive.Connection;
+
+namespace DuckDB.InteractiveExtension;
+
+public class ConnectDuckDBCommand : ConnectKernelCommand
+{
+    public ConnectDuckDBCommand()
+        : base("duckdb", "Connects to a DuckDB database")
+    {
+        Add(ConnectionStringArgument);
+    }
+
+    public Argument<string> ConnectionStringArgument { get; } =
+        new("connectionString", "The connection string used to connect to the database");
+
+    public override Task<Kernel> ConnectKernelAsync(
+        KernelInvocationContext context,
+        InvocationContext commandLineContext)
+    {
+        var connectionString = commandLineContext.ParseResult.GetValueForArgument(ConnectionStringArgument);
+        var connector = new DuckDBKernelConnector(connectionString);
+        var localName = commandLineContext.ParseResult.GetValueForOption(KernelNameOption);
+        return connector.CreateKernelAsync(localName);
+    }
+}
---
+++ src/DuckDB.InteractiveExtension/KernelExtension.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.DotNet.Interactive;
+
+namespace DuckDB.InteractiveExtension;
+
+public class KernelExtension
+{
+    public static void Load(Kernel kernel)
+    {
+        if (kernel is CompositeKernel compositeKernel)
+        {
+            compositeKernel
+                .AddKernelConnector(new ConnectDuckDBCommand());
+
+            KernelInvocationContext.Current?.Display(
+                new HtmlString(@"<details><summary>Query DuckDB databases.</summary>
+    <p>This extension adds support for connecting to DuckDB databases using the <code>#!connect duckdb</code> magic command. </p>
+    </details>"),
+                "text/html");
+        }
+    }
+}
